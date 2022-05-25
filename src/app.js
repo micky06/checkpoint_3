b95bb0bf07021756e8b6ec302ee5a73c9cd99b4d
michael <michael-michaux_student2021@wilder.school>
2022-05-25 09:16:51 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+// Please keep this module.exports app, we need it for the tests !
+module.exports = app;
